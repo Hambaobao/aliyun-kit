b4cee669de22b9d0e3bce78eaf9b829c91c07b4d
Hambaobao <jameszhang2880@gmail.com>
2025-09-13 03:49:24 +0800

    Update version to 0.2.1 in pyproject.toml and add ClientException to __all__ in __init__.py

--- aliyun/__init__.py
+++ aliyun/__init__.py
@@ -1,4 +1,6 @@
+from alibabacloud_tea_openapi.exceptions import ClientException
+
 from aliyun.acr.base import AcrClient
 from aliyun.ecs.base import EcsClient
 
-__all__ = ["AcrClient", "EcsClient"]
+__all__ = ["AcrClient", "EcsClient", "ClientException"]

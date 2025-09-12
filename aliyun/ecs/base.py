2ad36316a95f84ce698dc495db5bd60f73b4e223
Hambaobao <jameszhang2880@gmail.com>
2025-09-13 03:35:30 +0800

    Refactor ECS and ACR clients: remove EcsInstance class, consolidate rate limiting and retry logic, and introduce constants for zone and region mappings. Update method signatures for clarity and remove unused imports.

--- aliyun/ecs/base.py
+++ aliyun/ecs/base.py
@@ -1,6 +1,5 @@
 import os
 import json
-import asyncio
 
 from typing import Dict, List, Literal
 
@@ -9,9 +8,7 @@ from alibabacloud_ecs20140526.client import Client as Ecs20140526Client
 from alibabacloud_tea_openapi import models as open_api_models
 from alibabacloud_tea_util import models as util_models
 
-from aliyun.ecs.constants import REGION_ID_TO_SECURITY_GROUP_ID, ZONE_ID_TO_V_SWITCH_ID
-from aliyun.utils.rate_limit import rate_limit
-from aliyun.utils.retry import retry_decorator
+from aliyun.constants import REGION_ID_TO_SECURITY_GROUP_ID, ZONE_ID_TO_V_SWITCH_ID
 
 
 class EcsClient:
@@ -24,6 +21,10 @@ class EcsClient:
             security_group_id: str = None,
             v_switch_id: str = None,
     ):
+        """
+        Initialize the ECS client
+        """
+
         self.region_id = region_id
         self.zone_id = zone_id
         self.resource_group_id = resource_group_id
@@ -37,6 +38,10 @@ class EcsClient:
         self.client = self.create_client()
 
     def create_client(self) -> Ecs20140526Client:
+        """
+        Create the ECS client
+        """
+
         config = open_api_models.Config(
             access_key_id=os.environ['ALIBABA_CLOUD_ACCESS_KEY_ID'],
             access_key_secret=os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET'],
@@ -44,8 +49,6 @@ class EcsClient:
         config.endpoint = f'ecs.{self.region_id}.aliyuncs.com'
         return Ecs20140526Client(config)
 
-    @retry_decorator()
-    @rate_limit()
     async def create_instances_async(
         self,
         instance_name: str,
@@ -60,7 +63,10 @@ class EcsClient:
         internet_max_bandwidth_out: int = 100,
         internet_max_bandwidth_in: int = 100,
         amount: int = 1,
-    ) -> List[str] | None:
+    ) -> List[str]:
+        """
+        Create ECS instances.
+        """
 
         def create_data_disk_request() -> List[ecs_20140526_models.RunInstancesRequestDataDisk] | None:
             if data_disk_size is None:
@@ -103,74 +109,50 @@ class EcsClient:
             v_switch_id=self.v_switch_id,
         )
         runtime = util_models.RuntimeOptions()
-        try:
-            response = await self.client.run_instances_with_options_async(run_instances_request, runtime)
-            if response.status_code == 200:
-                instance_ids = [instance_id for instance_id in response.body.instance_id_sets.instance_id_set]
-                return instance_ids
-            else:
-                return None
-        except Exception as error:
-            print(error)
-            return None
+        response = await self.client.run_instances_with_options_async(run_instances_request, runtime)
+        instance_ids = [instance_id for instance_id in response.body.instance_id_sets.instance_id_set]
+        return instance_ids
 
-    @retry_decorator()
-    @rate_limit()
-    async def describe_instance_status_async(self, instance_id: str, region_id: str = None) -> Dict[str, str] | None:
-        client = self.create_client()
+    async def describe_instance_status_async(self, instance_id: str, region_id: str = None) -> Dict[str, str]:
+        """
+        Describe the status of an ECS instance.
+        """
+
         describe_instance_status_request = ecs_20140526_models.DescribeInstanceStatusRequest(
             region_id=region_id or self.region_id,
             instance_id=[instance_id],
         )
         runtime = util_models.RuntimeOptions()
-        try:
-            response = await client.describe_instance_status_with_options_async(describe_instance_status_request, runtime)
-            if response.status_code == 200:
-                return {
-                    "status": response.body.instance_statuses.instance_status[0].status,
-                }
-            else:
-                return None
-        except Exception as error:
-            print(error)
-            return None
+        response = await self.client.describe_instance_status_with_options_async(describe_instance_status_request, runtime)
+        return {
+            "status": response.body.instance_statuses.instance_status[0].status,
+        }
 
-    @retry_decorator()
-    @rate_limit()
     async def describe_instance_attribute_async(
         self,
         instance_id: str,
-    ) -> ecs_20140526_models.DescribeInstanceAttributeResponseBody | None:
-        client = self.create_client()
+    ) -> ecs_20140526_models.DescribeInstanceAttributeResponseBody:
+        """
+        Describe the attributes of an ECS instance.
+        """
+
         describe_instance_attribute_request = ecs_20140526_models.DescribeInstanceAttributeRequest(instance_id=instance_id)
         runtime = util_models.RuntimeOptions()
-        try:
-            response = await client.describe_instance_attribute_with_options_async(
-                describe_instance_attribute_request,
-                runtime,
-            )
-            if response.status_code == 200:
-                return response.body
-            else:
-                return None
-        except Exception as error:
-            print(error)
-            return None
+        response = await self.client.describe_instance_attribute_with_options_async(
+            describe_instance_attribute_request,
+            runtime,
+        )
+        return response.body
 
-    async def describe_instances_async(self, instance_ids: List[str]) -> List[ecs_20140526_models.DescribeInstancesResponseBodyInstancesInstance] | None:
+    async def describe_instances_async(self, instance_ids: List[str]) -> List[ecs_20140526_models.DescribeInstancesResponseBodyInstancesInstance]:
         describe_instances_request = ecs_20140526_models.DescribeInstancesRequest(
             region_id=self.region_id,
             instance_ids=json.dumps(instance_ids),
         )
         runtime = util_models.RuntimeOptions()
-        try:
-            response = await self.client.describe_instances_with_options_async(describe_instances_request, runtime)
-            return response.body.instances.instance if response.status_code == 200 else None
-        except Exception as error:
-            print(error)
+        response = await self.client.describe_instances_with_options_async(describe_instances_request, runtime)
+        return response.body.instances.instance
 
-    @retry_decorator()
-    @rate_limit()
     async def run_command_async(
         self,
         instance_id: str | List[str],
@@ -180,8 +162,11 @@ class EcsClient:
         region_id: str = None,
         command_type: str = "RunShellScript",
         timeout: int = 3600,
-    ) -> Dict[str, str] | None:
-        client = self.create_client()
+    ) -> ecs_20140526_models.RunCommandResponseBody:
+        """
+        Run a command on an ECS instance.
+        """
+
         _instance_id = instance_id if isinstance(instance_id, list) else [instance_id]
         run_command_request = ecs_20140526_models.RunCommandRequest(
             region_id=region_id or self.region_id,
@@ -194,18 +179,9 @@ class EcsClient:
             timeout=timeout,
         )
         runtime = util_models.RuntimeOptions()
-        try:
-            response = await client.run_command_with_options_async(run_command_request, runtime)
-            return {
-                "command-id": response.body.command_id,
-                "invoke-id": response.body.invoke_id,
-            } if response.status_code == 200 else None
-        except Exception as error:
-            print(error)
-            return None
+        response = await self.client.run_command_with_options_async(run_command_request, runtime)
+        return response.body
 
-    @retry_decorator()
-    @rate_limit()
     async def describe_invocation_result_async(
         self,
         instance_id: str,
@@ -213,8 +189,11 @@ class EcsClient:
         invoke_id: str,
         region_id: str = None,
         content_encoding: str = "PlainText",
-    ) -> ecs_20140526_models.DescribeInvocationResultsResponseBodyInvocationInvocationResultsInvocationResult | None:
-        client = self.create_client()
+    ) -> ecs_20140526_models.DescribeInvocationResultsResponseBodyInvocationInvocationResultsInvocationResult:
+        """
+        Describe the results of an invocation on an ECS instance.
+        """
+
         describe_invocation_results_request = ecs_20140526_models.DescribeInvocationResultsRequest(
             region_id=region_id or self.region_id,
             command_id=command_id,
@@ -224,11 +203,9 @@ class EcsClient:
             content_encoding=content_encoding,
         )
         runtime = util_models.RuntimeOptions()
-        response = await client.describe_invocation_results_with_options_async(describe_invocation_results_request, runtime)
+        response = await self.client.describe_invocation_results_with_options_async(describe_invocation_results_request, runtime)
         return response.body.invocation.invocation_results.invocation_result[0]
 
-    @retry_decorator()
-    @rate_limit()
     async def create_disk_async(
         self,
         size: int,
@@ -236,7 +213,11 @@ class EcsClient:
         disk_category: str = "cloud_auto",
         snapshot_id: str = None,
         region_id: str = None,
-    ) -> ecs_20140526_models.CreateDiskResponseBody | None:
+    ) -> ecs_20140526_models.CreateDiskResponseBody:
+        """
+        Create a disk on an ECS instance.
+        """
+
         create_disk_request = ecs_20140526_models.CreateDiskRequest(
             region_id=region_id or self.region_id,
             zone_id=zone_id or self.zone_id,
@@ -246,36 +227,31 @@ class EcsClient:
             snapshot_id=snapshot_id,
         )
         runtime = util_models.RuntimeOptions()
-        try:
-            response = await self.client.create_disk_with_options_async(create_disk_request, runtime)
-            return response.body if response.status_code == 200 else None
-        except Exception as error:
-            print(error)
-            return None
+        response = await self.client.create_disk_with_options_async(create_disk_request, runtime)
+        return response.body
 
-    @retry_decorator()
-    @rate_limit()
-    async def reset_disk_async(self, disk_id: str, snapshot_id: str) -> str | None:
+    async def reset_disk_async(self, disk_id: str, snapshot_id: str) -> None:
+        """
+        Reset a disk on an ECS instance.
+        """
+
         reset_disk_request = ecs_20140526_models.ResetDiskRequest(
             disk_id=disk_id,
             snapshot_id=snapshot_id,
         )
         runtime = util_models.RuntimeOptions()
-        try:
-            response = await self.client.reset_disk_with_options_async(reset_disk_request, runtime)
-            return response.body.request_id if response.status_code == 200 else None
-        except Exception as error:
-            print(error)
-            return None
+        await self.client.reset_disk_with_options_async(reset_disk_request, runtime)
 
-    @retry_decorator()
-    @rate_limit()
     async def describe_disks_async(
         self,
         instance_id: str,
         disk_type: Literal["data", "system", "all"] = "data",
         region_id: str = None,
-    ) -> str | None:
+    ) -> ecs_20140526_models.DescribeDisksResponseBodyDisksDisk:
+        """
+        Describe the disks on an ECS instance.
+        """
+
         describe_disks_request = ecs_20140526_models.DescribeDisksRequest(
             region_id=region_id or self.region_id,
             instance_id=instance_id,
@@ -283,42 +259,37 @@ class EcsClient:
             resource_group_id=self.resource_group_id,
         )
         runtime = util_models.RuntimeOptions()
-        try:
-            response = await self.client.describe_disks_with_options_async(describe_disks_request, runtime)
-            return response.body.disks.disk[0].disk_id if response.status_code == 200 else None
-        except Exception as error:
-            print(error)
-            return None
+        response = await self.client.describe_disks_with_options_async(describe_disks_request, runtime)
+        return response.body.disks
 
-    @retry_decorator()
-    @rate_limit()
     async def attach_disk_async(
         self,
         disk_id: str,
         instance_id: str,
         delete_with_instance: bool = False,
-    ) -> ecs_20140526_models.AttachDiskResponseBody | None:
+    ) -> ecs_20140526_models.AttachDiskResponseBody:
+        """
+        Attach a disk to an ECS instance.
+        """
+
         attach_disk_request = ecs_20140526_models.AttachDiskRequest(
             instance_id=instance_id,
             disk_id=disk_id,
             delete_with_instance=delete_with_instance,
         )
         runtime = util_models.RuntimeOptions()
-        try:
-            response = await self.client.attach_disk_with_options_async(attach_disk_request, runtime)
-            return response.body if response.status_code == 200 else None
-        except Exception as error:
-            print(error)
-            return None
+        response = await self.client.attach_disk_with_options_async(attach_disk_request, runtime)
+        return response.body
 
-    @retry_decorator()
-    @rate_limit()
-    async def delete_instances_async(self, instance_id: str | List[str], region_id: str = None, amount_limit: int = 100) -> List[str] | None:
+    async def delete_instances_async(self, instance_id: str | List[str], region_id: str = None, amount_limit: int = 100) -> List[str]:
+        """
+        Delete ECS instances.
+        """
 
         _instance_id = instance_id if isinstance(instance_id, list) else [instance_id]
         deleted_instance_ids = []
-        _amount = len(_instance_id)
-        while _amount > 0:
+        num_left = len(_instance_id)
+        while num_left > 0:
             instance_ids_to_delete = _instance_id[:amount_limit]
             delete_instances_request = ecs_20140526_models.DeleteInstancesRequest(
                 region_id=region_id or self.region_id,
@@ -326,82 +297,67 @@ class EcsClient:
                 instance_id=instance_ids_to_delete,
             )
             runtime = util_models.RuntimeOptions()
-            try:
-                response = await self.client.delete_instances_with_options_async(delete_instances_request, runtime)
-                if response.status_code == 200:
-                    deleted_instance_ids.extend(instance_ids_to_delete)
-                    _amount -= len(instance_ids_to_delete)
-                    _instance_id = _instance_id[amount_limit:]
-                else:
-                    return None
-            except Exception as error:
-                print(error)
-                return None
+            await self.client.delete_instances_with_options_async(delete_instances_request, runtime)
+            deleted_instance_ids.extend(instance_ids_to_delete)
+            num_left -= len(instance_ids_to_delete)
+            _instance_id = _instance_id[amount_limit:]
         return deleted_instance_ids
 
-    @retry_decorator()
-    @rate_limit()
-    async def delete_instance_async(self, instance_id: str) -> str | None:
-        print(f"Deleting instance: {instance_id}")
+    async def delete_instance_async(self, instance_id: str) -> None:
+        """
+        Delete an ECS instance.
+        """
+
         delete_instance_request = ecs_20140526_models.DeleteInstanceRequest(
             instance_id=instance_id,
             force=True,
         )
         runtime = util_models.RuntimeOptions()
-        try:
-            response = await self.client.delete_instance_with_options_async(delete_instance_request, runtime)
-            if response.status_code == 200:
-                return instance_id
-            else:
-                return None
-        except Exception as error:
-            print(error)
-            return None
+        await self.client.delete_instance_with_options_async(delete_instance_request, runtime)
 
-    @retry_decorator()
-    @rate_limit()
-    async def create_image_and_wait_until_ready_async(self, instance_id: str, image_name: str) -> str | None:
-        try:
-            create_image_request = ecs_20140526_models.CreateImageRequest(
-                region_id=self.region_id,
-                instance_id=instance_id,
-                resource_group_id=self.resource_group_id,
-                image_name=image_name,
-            )
-            runtime = util_models.RuntimeOptions()
-            create_image_response = await self.client.create_image_with_options_async(create_image_request, runtime)
-            image_id = create_image_response.body.image_id
-            print(f"Create image for {instance_id}: {image_id = }")
+    async def create_image_async(self, instance_id: str, image_name: str) -> str:
+        """
+        Create an image on an ECS instance.
+        """
 
-            while True:
-                await asyncio.sleep(10)
-                describe_images_request = ecs_20140526_models.DescribeImagesRequest(region_id=self.region_id, resource_group_id=self.resource_group_id, image_id=image_id)
-                runtime = util_models.RuntimeOptions()
-                response = await self.client.describe_images_with_options_async(describe_images_request, runtime)
-                if "Available" in str(response.body):
-                    break
-                else:
-                    print(f"Wait for image {image_id} ready...")
-            return image_id
-        except Exception as error:
-            print(error)
-            return None
+        create_image_request = ecs_20140526_models.CreateImageRequest(
+            region_id=self.region_id,
+            instance_id=instance_id,
+            resource_group_id=self.resource_group_id,
+            image_name=image_name,
+        )
+        runtime = util_models.RuntimeOptions()
+        create_image_response = await self.client.create_image_with_options_async(create_image_request, runtime)
+        return create_image_response.body.image_id
+
+    async def describe_images_async(self, image_id: str) -> ecs_20140526_models.DescribeImagesResponseBodyImagesImage:
+        """
+        Describe the images on an ECS instance.
+        """
+
+        describe_images_request = ecs_20140526_models.DescribeImagesRequest(
+            region_id=self.region_id,
+            resource_group_id=self.resource_group_id,
+            image_id=image_id,
+        )
+        runtime = util_models.RuntimeOptions()
+        response = await self.client.describe_images_with_options_async(describe_images_request, runtime)
+        return response.body.images
 
     async def stop_invoke_async(
         self,
         instance_id: str,
         invoke_id: str,
         region_id: str = None,
-    ) -> str | None:
+    ) -> None:
+        """
+        Stop an invocation on an ECS instance.
+        """
+
         stop_invocation_request = ecs_20140526_models.StopInvocationRequest(
             region_id=region_id or self.region_id,
             instance_id=instance_id,
             invoke_id=invoke_id,
         )
         runtime = util_models.RuntimeOptions()
-        try:
-            response = await self.client.stop_invocation_with_options_async(stop_invocation_request, runtime)
-            return response.body.instance_statuses.instance_status[0].status if response.status_code == 200 else None
-        except Exception as error:
-            print(error)
-            return None
+        await self.client.stop_invocation_with_options_async(stop_invocation_request, runtime)
